edb2ae0df442dcfa45f2d93ebea6910a46c3f5dd
konradlisthrop <konradlisthrop@gmail.com>
2024-11-07 20:56:30 +0000

    Tasks

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,14 +1,25 @@
 from django.urls import path
-from .views import (
+from .project import (
     ProjectListView,
     ProjectCreateView,
     ProjectUpdateView,
     ProjectDeleteView,
 )
+from .task import (
+    TaskListView, 
+    TaskCreateView, 
+    TaskUpdateView, 
+    TaskDeleteView
+)
+
 
 urlpatterns = [
     path('', ProjectListView.as_view(), name='project_list'),
     path('create/', ProjectCreateView.as_view(), name='project_create'),
     path('edit/<int:pk>/', ProjectUpdateView.as_view(), name='project_edit'),
     path('delete/<int:pk>/', ProjectDeleteView.as_view(), name='project_delete'),
+    path('tasks/', TaskListView.as_view(), name='task-list'),
+    path('tasks/create/', TaskCreateView.as_view(), name='task-create'),
+    path('tasks/edit/<int:pk>/', TaskUpdateView.as_view(), name='task-edit'),
+    path('tasks/delete/<int:pk>/', TaskDeleteView.as_view(), name='task-delete'),
 ]
