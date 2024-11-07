cb2a2c5794ef99a68a540a01214dfa37f797d882
mtoledom <mtoledom@unemi.edu.ec>
2024-11-07 21:15:44 +0000

    formularios

--- mysite/mysite/forms.py
+++ mysite/mysite/forms.py
@@ -5,3 +5,14 @@ class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
         fields = ['nombre']
+
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['title', 'description', 'completed', 'category']
+
+class ProyectForm(forms.ModelForm):
+    class Meta:
+        model = Proyect
+        fields = ['nombre', 'description']
